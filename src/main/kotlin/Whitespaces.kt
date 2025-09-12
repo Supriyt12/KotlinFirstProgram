baba23a2708c609d879402ddc6261cfc2d7ca170
ACER <supriyantosupri0205@gmail.com>
2025-09-12 21:45:25 +0700

    Whitespaces.kt

---
+++ src/main/kotlin/Whitespaces.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Hello Word!")
+
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+    */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
